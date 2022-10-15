001b78c40f6dc929a4038ad313463c7ec9eb6f64
pedronunomacedo <pedronunomacedo@hotmail.com>
2022-10-15 21:10:11 +0100

    Initial commit

--- TP1/normalizePolynomials.hs
+++ TP1/normalizePolynomials.hs
@@ -1,11 +1,21 @@
 import Data.Char
 
+data Expo = Expo { var :: Char, exponent :: Integer }
+data Term = Term { signal :: Char, number :: Float, expos :: [Expo] }
+
+testExpo :: Char -> Integer -> Expo
+testExpo var exponent = Expo var exponent
+
+testTerm :: Char -> Float -> [Expo] -> Term
+testTerm signal number l = Term signal number l
+
+
 --------------- Divide the given array in string parts ---------------
 
 splitArray :: [Char] -> [[Char]]
 splitArray [] = [[]]
 splitArray [x] = []
-splitArray l = (splitPair l):(splitArray (makeArray l 0))
+splitArray l = if ((head l) == '-') then (splitPair l):(splitArray (makeArray l 0)) else (splitPair l):(splitArray (makeArray l 0))
 
 
 splitPair :: [Char] -> [Char]
@@ -24,7 +34,7 @@ makeArray (x:xs) num
   | ((((head xs) /= '+') || ((head xs) /= '-')) && num == 0) = makeArray xs 0
   | otherwise = [x] ++ (makeArray xs 1)
 
---------------- Transform the list of strings in [(num, letter, coef),...] ---------------
+--------------- Transform the list of strings in [(num, letter, grau),...] ---------------
 searchForTimes :: [Char] -> Bool
 searchForTimes [] = False
 searchForTimes (x:xs)
@@ -37,5 +47,21 @@ searchForPower (x:xs)
   | (x == '^') = True
   | otherwise = searchForPower xs
 
+readAfterTimes :: [Char] -> Int -> Int
+readAfterTimes [] _ = 0
+readAfterTimes [x] _ = digitToInt x
+readAfterTimes (x:xs) b
+  | (x == '*') = readAfterTimes xs 1
+  | (x /= '*' && b == 1) = (readAfterTimes xs 1) * 10 + (digitToInt x)
+  | otherwise = readAfterTimes xs 0
 
--- [(Char,Int),(Char,Int)]
+
+
+divideInTuples :: [Char] -> [Term]
+divideInTuples = makeTuples (splitArray l)
+
+makeTuples :: [[Char]] -> [Term]
+makeTuples (x:xs) = 
+  | (times == True) =
+  where times = searchForTimes x
+        power = searchForPower x
