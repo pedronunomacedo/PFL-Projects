d1ef1ca1e2ddb3e83ed52bb3bbe0dbb45e177dd5
gonzallito <goncaloferreira2408@gmail.com>
2022-10-17 15:31:55 +0100

    comme ci comme ça

--- TP1/normalizePolynomials.hs
+++ TP1/normalizePolynomials.hs
@@ -1,22 +1,31 @@
 import Data.Char
 
-data Expo = Expo { var :: Char, exponent :: Integer }
-data Term = Term { signal :: Char, number :: Float, expos :: [Expo] }
+data Expo = Expo { var :: Char, exponent :: Integer } deriving (Show)
+data Term = Term { number :: Float, expos :: [Expo] } deriving (Show)
 
 testExpo :: Char -> Integer -> Expo
 testExpo var exponent = Expo var exponent
 
-testTerm :: Char -> Float -> [Expo] -> Term
-testTerm signal number l = Term signal number l
+testTerm :: Float -> [Expo] -> Term
+testTerm number l = Term number l
 
 
 --------------- Divide the given array in string parts ---------------
+mainFunction :: [Char] -> [[Char]]
+mainFunction l
+  | (head newl == '+' || head newl == '-') = splitArray newl
+  | otherwise = splitArray ('+':newl)
+  where newl = (filter (\x -> (x /=' ')) l)
+
+{-
+mainFunction "0*x^2 + 2*y + 5*z + y + 7*y^2"
+["+0*x^2","+2*y","+5*z","+y","+7*y^2","+2"]
+-}
 
 splitArray :: [Char] -> [[Char]]
 splitArray [] = [[]]
 splitArray [x] = []
-splitArray l = if ((head l) == '-') then (splitPair l):(splitArray (makeArray l 0)) else (splitPair l):(splitArray (makeArray l 0))
-
+splitArray l = (splitPair l):(splitArray (makeArray l 0))
 
 splitPair :: [Char] -> [Char]
 splitPair [] = []
@@ -34,18 +43,38 @@ makeArray (x:xs) num
   | ((((head xs) /= '+') || ((head xs) /= '-')) && num == 0) = makeArray xs 0
   | otherwise = [x] ++ (makeArray xs 1)
 
+takeExpoNum :: [Char] -> [Char]
+takeExpoNum [x] = [x]
+takeExpoNum (x:xs) = x:(takeExpoNum xs)
+
+--"4*x^2*y^2" [Expo = {var: x, expoente: 2}, Expo = {var: y, expoente: 2}]
+makeExpos :: [Char] -> [Expo]
+makeExpos l = [inExpo ( takeWhile (/='*') (tail (dropWhile (/= '*') l)) )]++[inExpo ( dropWhile (/='*') (tail(dropWhile (/= '*') l)) )]
+
+inExpo :: [Char] -> Expo
+inExpo [] = Expo ' ' 0
+inExpo [x] = Expo x 1
+inExpo (x:y)
+  | (isAlpha x) = if (head y == '^') then Expo x (read (takeExpoNum (tail y))::Integer) else Expo x 1
+  | otherwise = inExpo y
+
+takeNum :: [Char] -> [Char]
+takeNum [x] = [x]
+takeNum x = takeWhile (/='*') x
+
+inTerm :: [Char] -> Term
+inTerm [] = Term 0 []
+inTerm [x] = Term (read [x] :: Float) []
+inTerm x = Term (read (takeNum x) :: Float) []
+
+
+
 --------------- Transform the list of strings in [(num, letter, grau),...] ---------------
 searchForTimes :: [Char] -> Bool
-searchForTimes [] = False
-searchForTimes (x:xs)
-  | (x == '*') = True
-  | otherwise = searchForTimes xs
+searchForTimes str = if (length (filter (=='*') str) /= 0) then True else False
 
 searchForPower :: [Char] -> Bool
-searchForPower [] = False
-searchForPower (x:xs)
-  | (x == '^') = True
-  | otherwise = searchForPower xs
+searchForPower str = if (length (filter (=='^') str) /= 0) then True else False
 
 readAfterTimes :: [Char] -> Int -> Int
 readAfterTimes [] _ = 0
@@ -54,14 +83,3 @@ readAfterTimes (x:xs) b
   | (x == '*') = readAfterTimes xs 1
   | (x /= '*' && b == 1) = (readAfterTimes xs 1) * 10 + (digitToInt x)
   | otherwise = readAfterTimes xs 0
-
-
-
-divideInTuples :: [Char] -> [Term]
-divideInTuples = makeTuples (splitArray l)
-
-makeTuples :: [[Char]] -> [Term]
-makeTuples (x:xs) = 
-  | (times == True) =
-  where times = searchForTimes x
-        power = searchForPower x
